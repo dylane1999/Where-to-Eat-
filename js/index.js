6da68ee4053c7196ba352299c7bbc440ab283aec
Nathan Engstrom <naengstrom@davidson.edu>
2019-08-03 21:45:37 -0700

    object restaurants with list of ritual restaurants

--- js/index.js
+++ js/index.js
@@ -1,7 +1,86 @@
+let restaurants = [
+  (shanghaiChina = {
+    name: "Shanghai China Restuarant",
+    walkTime: 5
+  }),
 
-[rest1,2,3,4]
+  (elCapitanTaqueria = {
+    name: "El Capitan Taqueria",
+    walkTime: 8
+  }),
 
-function linkPicker(){
+  (mavelous = {
+    name: "Mavelous Coffee Bar & Little Griddle",
+    walkTime: 3
+  }),
 
+  (pokeBar = {
+    name: "Poke Bar",
+    walkTime: 3
+  }),
 
-}
+  (cumaica = {
+    name: "Cumaica Coffee",
+    walkTime: 2
+  }),
+
+  (otoroSushi = {
+    name: "Otoro Sushi",
+    walkTime: 13
+  }),
+
+  (manilaBowl = {
+    name: "Manila Bowl",
+    walkTime: 3
+  }),
+
+  (fang = {
+    name: "Fang",
+    walkTime: 10
+  }),
+
+  (taqueriaCazadores = {
+    name: "Taqueria Cazadores",
+    walkTime: 5
+  }),
+
+  (grandmas = {
+    name: "Grandma's Deli & Cafe",
+    walkTime: 5
+  }),
+
+  (spiceOfAmerica = {
+    name: "Spice of America",
+    walkTime: 11
+  }),
+
+  (Kagawa = {
+    name: "Kagawa-ya Udon Noodle Company",
+    walkTime: 5
+  }),
+
+  (organicCoup = {
+    name: "The Organic Coup",
+    walkTime: 3
+  }),
+
+  (cadillac = {
+    name: "Cadillac Bar & Grill",
+    walkTime: 5
+  }),
+
+  (antons = {
+    name: "Anton's Pizza & Deli",
+    walkTime: 8
+  }),
+
+  (grove = {
+    name: "The Grove",
+    walkTime: 10
+  }),
+
+  (lersRosThai = {
+    name: "Lers Ros Thai",
+    walkTime: 11
+  })
+];
